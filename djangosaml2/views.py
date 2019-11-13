f1b25ac63dbaf111a5c23ad5f9bb6f1b38467874
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-11-13 18:20:13 +0100

    Update views.py

--- djangosaml2/views.py
+++ djangosaml2/views.py
@@ -142,7 +142,7 @@ def login(request,
 
     kwargs = {}
     # pysaml needs a string otherwise: "cannot serialize True (type bool)"
-    if getattr(conf, '_sp_force_authn'):
+    if getattr(conf, '_sp_force_authn', False):
         kwargs['force_authn'] = "true"
 
     if getattr(conf, '_sp_allow_create', 'false'):
